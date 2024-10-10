e5165c68ca408d1c6a945fe3b1d0eb4db84a3bd2
LatenT-BeacOn <mayamoni930@gmail.com>
2024-10-10 22:42:59 +0600

    added enent handler

--- project1/js/login.js
+++ project1/js/login.js
@@ -1,1 +1,15 @@
-console.log('button clicking file added');
+//console.log('button clicking file added');
+//search : form submit reloading the page;(again);
+
+document.getElementById('btn-login').addEventListener('click',function(event){
+    event.preventDefault();
+//   console.log('login button clicked');
+
+
+//get the phone 
+const phoneNum=document.getElementById('phone').value;
+console.log(phoneNum)
+
+
+
+})
