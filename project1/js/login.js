5cadff70128b1537a6acf3d9503b3a2246aca684
LatenT-BeacOn <mayamoni930@gmail.com>
2024-10-11 00:01:59 +0600

    welcome homepage

--- project1/js/login.js
+++ project1/js/login.js
@@ -1,15 +1,53 @@
 //console.log('button clicking file added');
 //search : form submit reloading the page;(again);
 
+// value-> from input Field;
+// innerText->from text area
+
+
+// document.getElementById('btn-login').addEventListener('click',function(event){
+//     event.preventDefault();
+// //   console.log('login button clicked');
+
+
+// //get the phone 
+// const phoneNum=document.getElementById('phone').value;
+
+// const pinNum=document.getElementById('pin-num').value;
+// console.log(phoneNum,pinNum);
+
+
+// if(phoneNum==='5' && pinNum==='123'){
+//   console.loge('Your logged in');
+
+// }
+
+// else {
+//     alert('incorrect phone/pin');
+// }
+
+// })
+
+
+
+
 document.getElementById('btn-login').addEventListener('click',function(event){
-    event.preventDefault();
-//   console.log('login button clicked');
+event.preventDefault();
 
-
-//get the phone 
+//get phone;
 const phoneNum=document.getElementById('phone').value;
-console.log(phoneNum)
+const pinNum=document.getElementById('pin-num').value;
 
+if(phoneNum=== '5' && pinNum==='123'){
+    console.log('you are logged in');
+    window.location.href='home.html';
+   
+   
+    // window.location.href = '';
 
+}
+else {
+    alert('Wrong pin/phone');
+}
 
-})
+});
